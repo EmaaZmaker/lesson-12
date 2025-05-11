b1eb47d6aea4d0954aeef63b23b757e7442611fe
EmaaZmaker <emaazbinammar@gmail.com>
2025-05-11 10:38:57 +0400

    i dont understand

--- appy4.py
+++ appy4.py
@@ -2,5 +2,6 @@ n=int(input("enter the number"))
 num=int(input("enter the number you want to divide it with"))
 for i in range (n):
                 print("the answer is:",n/num)
+#mam i dont understand how to like stop the answer from repeating
 
     
